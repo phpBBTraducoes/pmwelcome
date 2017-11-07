ba0423f2e233b6ad908e28e348e9647a4d0358d8
phpBBTraducoes <eunaumtenhoid@outlook.com>
2017-11-07 21:44:57 -0200

    Traducao ADD

---
+++ language/pt_br/info_acp_pmwelcome.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+*
+* @package PM Welcome [Brazilian Portuguese [pt_br]] 
+* @copyright BB3.MOBi (c) 2015 Anvar http://apwa.ru
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Brazilian Portuguese  translation by null2 (c) 2016 [ver 1.0.1] (https://github.com/phpBBTraducoes)
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_PMWELCOME'					=> 'Mensagem de Boas-Vindas',
+	'ACP_PMWELCOME_EXPLAIN'			=> 'Você pode definir o texto da Mensagem Privada que será enviada ao usuário em seu primeiro acesso.',
+	'ACP_PMWELCOME_SETTINGS'		=> 'Configuração da mensagem de Boas-Vindas.',
+	'ACP_PMWELCOME_USER'			=> 'Remetente',
+	'ACP_PMWELCOME_USER_EXPLAIN'	=> 'Coloque a identificação do usuário remetente da Mensagem Privada.',
+	'ACP_PMWELCOME_SUBJECT'			=> 'Assunto da mensagem',
+	'ACP_PMWELCOME_TEXT'			=> 'Texto da mensagem',
+	'ACP_PMWELCOME_TEXT_EXPLAIN'	=> 'Você pode usar bbcodes, smiles e a chave {USERNAME} para o nome do usuário que receberá a mensagem.',
+));
